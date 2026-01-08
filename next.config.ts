38bcd96311b5232094e696ce88b174531cc8c83c
r.roux <roman.roux@student.isae-supaero.fr>
2026-01-08 09:11:30 +0100

    UP

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,20 @@
 import type { NextConfig } from "next";
 
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "X-Frame-Options", value: "SAMEORIGIN" },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" }
+        ],
+      },
+    ];
+  },
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
